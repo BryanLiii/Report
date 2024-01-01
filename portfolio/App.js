268f29233efb8ef95334f72dba3153f5c21095a9
BryanLiii <154072758+BryanLiii@users.noreply.github.com>
2024-01-01 17:11:15 +0900

    Add files via upload

---
+++ portfolio/App.js
@@ -0,0 +1,53 @@
+const topnav = document.querySelectorAll('.top-nav li')
+// Topbar
+topnav.forEach(function(item){
+    
+    item.addEventListener('click', function(e){
+
+        const {tagName, dataset} = e.target
+
+        if(tagName == 'A' && dataset.id == 1){
+            window.scrollTo({
+                top: document.querySelector('#about-me').offsetTop,
+                behavior: 'smooth' 
+            })
+        }else if(tagName == 'A' && dataset.id == 2){
+            window.scrollTo({
+                top: document.querySelector('#experience-section').offsetTop,
+                behavior: 'smooth'
+            })
+        }else if(tagName == 'A' && dataset.id == 3){
+            window.scrollTo({
+                top: document.querySelector('#projects-section').offsetTop,
+                behavior: 'smooth'
+            })
+        }else if(tagName == 'A' && dataset.id == 4){
+            window.scrollTo({
+                top: document.querySelector('#contact-section').offsetTop,
+                behavior: 'smooth'
+            })
+        }
+    })
+})
+
+// Download CV
+const cv = document.querySelector('.btn-containers .btn-cv')
+cv.addEventListener('click',() => {
+    window.open('/portfolio/Sources/履歴書-李雪剣.pdf')
+})
+const btnInfo = document.querySelector('.btn-containers .btn-info')
+btnInfo.addEventListener('click', () => {
+    window.open('/portfolio/Sources/職務経歴書-李雪剣.pdf')
+})
+
+
+// project section
+const ckbtn = document.querySelector('.check-it')
+ckbtn.addEventListener('click',() => {
+    location.href = '/calculator/Index.html'
+})
+
+// footer
+document.querySelector('.footer-section a').addEventListener('click', () => {
+    document.documentElement.scrollTop = 0
+})
